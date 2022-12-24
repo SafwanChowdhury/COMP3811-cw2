a539e95473a82baf4221c36a320b3028a79b49f5
SafwanChowdhury <72044721+SafwanChowdhury@users.noreply.github.com>
2022-12-24 00:16:38 +0000

    Update loadobj.cpp

--- main/loadobj.cpp
+++ main/loadobj.cpp
@@ -7,7 +7,7 @@ SimpleMeshData load_wavefront_obj( char const* aPath, Mat44f aPreTransform)
 {
 	auto result = rapidobj::ParseFile(aPath);
 	if (result.error)
-		throw Error("Unable to load OBJ file ’%s’: %s", aPath, result.error.code.message().c_str());
+		throw Error("Unable to load OBJ file â€™%sâ€™: %s", aPath, result.error.code.message().c_str());
 	rapidobj::Triangulate(result);
 
 	SimpleMeshData ret;
@@ -29,10 +29,12 @@ SimpleMeshData load_wavefront_obj( char const* aPath, Mat44f aPreTransform)
 				result.attributes.normals[idx.normal_index * 3 + 2],
 				});
 
-			ret.texcoords.emplace_back(Vec2f{
-			        result.attributes.texcoords[idx.texcoord_index * 3 + 0],
-				result.attributes.texcoords[idx.texcoord_index * 3 + 1],
-			  });
+			if (result.attributes.texcoords.size() != 0) {
+				ret.texcoords.emplace_back(Vec2f{
+					result.attributes.texcoords[idx.texcoord_index * 2 + 0],
+					result.attributes.texcoords[idx.texcoord_index * 2 + 1]
+			  	});
+			}
 			
 			auto const& mat = result.materials[shape.mesh.material_ids[i / 3]];
 			ret.colors.emplace_back(Vec3f{
